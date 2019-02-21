2ab2ff90a800c9615a2b27f08044cf2762e21050
Tyler Lemon <tlemon@dsg-c-linux1.jlab.org>
2019-02-21 08:57:54 -0500

    fix spacing on maps

--- hv_map.py
+++ hv_map.py
@@ -1,20 +1,4 @@
-#!/usr/bin/env python
-
-# Date:		January 28, 2019
-# Author:	Tyler Lemon
-
-#Change log:
-# Date,	Changed by:,	Change
-#2019-01-28,	Tyler Lemon,	Completion of initial development
-
-
-# "hv_map.py" replicates TCL/TK's perl script of the same name used by
-# Hall C to create group_map, channel_map, and HV.alhConfig files from HV.hvc.
-# Resulting files of this python program are same as perl script with the
-# exception of 1.) crate and group headers in channel_map and group_map were 
-# changed to fit entire group name and fix spacing issues and 
-# 2.) In .alhConfig file, channels are listed in increasing numerical order
-# (side effect of reusing logic used to split crates and channels up).
+#!/usr/env python
 
 import sys # used to check input arguements and exit program on error
 import os # used to check whether .hvc and .group file exist
@@ -22,7 +6,8 @@ import array # used to count number of channels in each group
 from math import ceil # used to calculate number of pages in output files.
 from datetime import datetime # used to get timestamp for page headers
 
-
+alhOnly = False
+outPath = ''
 
 # Checks if there are any input arguements, assigns prefix and spec if so.
 if len(sys.argv)-1 == 0:
@@ -31,6 +16,7 @@ elif len(sys.argv)-1 == 1:
 	prefix,spec = sys.argv[1],'Hall C'
 elif len(sys.argv)-1 >= 2:
 	prefix,spec = sys.argv[1],sys.argv[2]
+
 
 # Declares input configuration files and output file name
 hvConfig = prefix+'.hvc'
@@ -55,41 +41,47 @@ for line in configLines:
 		groups.append(int(group))
 		if crate not in prevCrate:
 			prevCrate.append(crate)
-			print('Found new crate... number '+crate)
+			if not alhOnly:
+				print('Found new crate... number '+crate)
 		if [crate,slot,channel.zfill(2)] not in used:
 			used.append([crate,slot,channel.zfill(2)])
 		else:
-			print('>>>'+'\033[1m'+'\tMultiply assigned HV channel!'+'\033[0m'+\
-				'\n>>>\tCrate/Slot/Channel '+crate+'/'+slot+'/'+channel+\
-				' already assigned to '+'\033[1m'+\
+			print('>>>'+'\033[1m'+'\tMultiply assigned HV channel!'+\
+				'\033[0m'+'\n>>>\tCrate/Slot/Channel '+crate+'/'+\
+				slot+'/'+channel+' already assigned to '+'\033[1m'+\
 				chs[used.index([crate,slot,channel])][0]+'\033[0m'+\
-				'.\n>>>\tAttempted assignment to '+'\033[1m'+label+'\033[0m'+\
-				' has been ignored.\n>>>')
+				'.\n>>>\tAttempted assignment to '+'\033[1m'+label+\
+				'\033[0m'+' has been ignored.\n>>>')
 
-# Reads in HV.group and prints each group with ID number and number of channels
+# Reads in HV.group and prints each group with ID number and number of
+# channels
 if (os.path.isfile(groupFile)):
-	print('Opening group file '+groupFile)
+	if not alhOnly:
+		print('Opening group file '+groupFile)
 	try:
 		with open(groupFile,'r') as f:
 			groupLines = f.readlines()
 	except:
 		print('Cannot open config file: '+groupFile+'.\n')
 		sys.exit(0)
-print('Group Information:')
+if not alhOnly:
+	print('Group Information:')
 grNames = []
 for line in groupLines:
 	if line[0] != '#' and line[0] != '\n':
 		grID = line.strip().split(' ')[0]
 		grName = ' '.join(line.strip().split(' ')[1:])
 		grNames.append([grID,grName])
-		print('\tGroup '+grName+' (id '+grID+')'+' has '+\
-			str(groups.count(int(grID)))+' channels')
+		if not alhOnly:
+			print('\tGroup '+grName+' (id '+grID+')'+' has '+\
+				str(groups.count(int(grID)))+' channels')
 
 
 # Creates group_map file
 date = datetime.now().strftime('%X %a %b %d %Y')
-pgHeader = ('Group Contents Map Generated '+date).center(80)[:len((\
-	'Group Contents Map Generated '+date).center(80))-len('Page:N')]+'Page:'
+pgHeader = ('Group Contents Map Generated '+date).center(87)[:len((\
+	'Group Contents Map Generated '+date).center(87))-len('Page:N')]+\
+    'Page:'
 
 groupLines.sort()
 
@@ -98,28 +90,27 @@ for group in groupLines:
 	grID = group.strip().split(' ')[0]
 	grName = ' '.join(group.strip().split(' ')[1:])
 	if len(grName) > 18:
-		hold = ['_'*19+'|',('Group '+grID).center(19)+'|',\
-			grName[:18].center(19)+'|',grName[18:].center(19)+'|',\
-			'_'*19+'|',' '*19+'|']
+		hold = ['_'*21+'|',('Group '+grID).center(21)+'|',\
+			grName[:18].center(21)+'|',grName[18:].center(21)+'|',\
+			'_'*21+'|',' '*21+'|']
 	else:
-		hold = ['_'*19+'|',('Group '+grID).center(19)+'|',\
-			grName.center(19)+'|',' '*19+'|','_'*19+'|',' '*19+'|']
+		hold = ['_'*21+'|',('Group '+grID).center(21)+'|',\
+			grName.center(21)+'|',' '*21+'|','_'*21+'|',' '*21+'|']
 	for ch in chs:
 		chID,cr,sl,chn,gr = ch
 		chn = chn.zfill(2)
 		if gr == str(grID):
-			spacing = 17-len(chID)-len(cr+'/'+sl+'/'+chn)
+			spacing = 19-len(chID)-len(cr+'/'+sl+'/'+chn)
 			line = ' '+chID+' '*spacing+cr+'/'+sl+'/'+chn+' |'
-			if len(line) > 20:
-				line = line[: -2]+'|'
 			hold.append(line)
-	hold.append(' '*19+'|')
+	hold.append(' '*21+'|')
 	mapping.append(hold)
 
+
 numPgs = int(ceil(len(groupLines)/4.0))
-start,final = 0,[]
+final = []
 for page in range(0,numPgs):
-	pg = mapping[start*page:start*page+4]
+	pg = mapping[4*page:4*page+4]
 	maxLen = 0
 	for item in pg:
 		if len(item) > maxLen:
@@ -128,21 +119,23 @@ for page in range(0,numPgs):
 		if len(item) < maxLen:
 			diff = maxLen - len(item)
 			for q in range(0,diff):
-				pg[i].append(' '*19+'|')
-	start += 4
+				pg[i].append(' '*21+'|')
 	if page != 0:
-		final.append('\f')
-	final.append(pgHeader+str(page+1))
+		final.append('\f'+pgHeader+str(page+1))
+	else:
+		final.append(pgHeader+str(page+1))
+
 	for i in range(0,len(pg[0])):
-		line = '|'	
+		line = '|'
 		for grp in pg:
 			line += grp[i]
 		final.append(line)
 
-with open('group_map','w') as f:
-	for line in final:
-		f.write(line)
-		f.write('\n')
+if not alhOnly:
+	with open(outPath+'group_map','w') as f:
+		for line in final:
+			f.write(line)
+			f.write('\n')
 
 
 # Creates channel_map file
@@ -162,41 +155,42 @@ for crate in prevCrate:
 			maxSl = int(ch[1])
 	chMap.append(hold)
 
-pgHeader = ('Channel Map Generated '+date).center(80)[:len((\
-	'Channel Contents Map Generated '+date).center(80))-len('Page:N')]+'Page:'
+pgHeader = ('Channel Map Generated '+date).center(87)[:len((\
+	'Channel Contents Map Generated '+date).center(87))-\
+	len('Page:N')]+'Page:'
 crateMap = []
 for crNum,crate in enumerate(chMap):
-	hold = ['_'*19+'|',('Crate '+str(crNum+1)).center(19)+'|','_'*19+'|',]
+	hold = ['_'*21+'|',('Crate '+str(crNum+1)).center(21)+'|','_'*21+'|',]
 	for slot in range(0,maxSl+1):
 		for ch in range(0,maxCh+1):
 			chInfo = str(slot)+'/'+str(ch).zfill(2)
 			try:
 				chID = labels[used.index([str(crNum+1),str(slot),\
 					str(ch).zfill(2)])]
-				hold.append(' '+chInfo.ljust(6)+chID.center(12)+'|')
+				hold.append(' '+chInfo.ljust(6)+chID.center(14)+'|')
 			except:
-				hold.append((' '+chInfo).ljust(19)+'|')
+				hold.append((' '+chInfo).ljust(21)+'|')
 	crateMap.append(hold)
 
 numPgs = int(ceil(len(crateMap)/4.0))
-start,final = 0,[]
+final = []
 for page in range(0,numPgs):
-	pg = crateMap[start*page:start*page+4]
-	start += 4
+	pg = crateMap[4*page:4*page+4]
 	if page != 0:
-		final.append('\f')
-	final.append(pgHeader+str(page+1))
+		final.append('\f'+pgHeader+str(page+1))
+	else:
+		final.append(pgHeader+str(page+1))
 	for i in range(0,len(pg[0])):
-		line = '|'	
+		line = '|'
 		for grp in pg:
 			line += grp[i]
 		final.append(line)
 
-with open('channel_map','w') as f:
-	for line in final:
-		f.write(line)
-		f.write('\n')
-
+if not alhOnly:
+	with open(outPath+'channel_map','w') as f:
+		for line in final:
+			f.write(line)
+			f.write('\n')
 
 # Creates .alhConfig file
 specNoSpace = spec.replace(' ','_')
@@ -217,5 +211,6 @@ for grp in groupLines:
 				pvBase+',title='+ch[0]+',address='+ch[1]+'/'+ch[2]+'/'+\
 				ch[3].zfill(2)+'" HV_alarm_set.edl >> /dev/null\n\n'
 
-with open(alhFile,'w') as f:
+with open(outPath+alhFile,'w') as f:
 	f.write(final)
+
